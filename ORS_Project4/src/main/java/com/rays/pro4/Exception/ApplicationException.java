fb3701e669349cde88c42d49ef23e534fd0bf3f5
Shivam3099 <shivampatel301999@gmail.com>
2022-08-04 18:13:18 +0530

    success

---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/ApplicationException.java
@@ -0,0 +1,16 @@
+package com.rays.pro4.Exception;
+
+/**
+ * ApplicationException is propogated from Service classes when an business
+ * logic exception occurered.
+ * 
+ * @author SHIVAM PATEL
+ *
+ */
+public class ApplicationException extends Exception {
+
+	public ApplicationException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ ORS_Project4/src/main/java/com/rays/pro4/Exception/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rays.pro4.Exception;
+
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * 
+ * @author SHIVAM PATEL
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+	public RecordNotFoundException(String msg) {
+		super(msg);
+	}
+}
